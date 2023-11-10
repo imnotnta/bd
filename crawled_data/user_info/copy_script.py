0608eeecc91ab70b59cd6af5c4de4726552ce90c
huydsai02 <huydsai02@gmail.com>
2023-11-10 23:28:54 +0700

    update

--- crawled_data/user_info/copy_script.py
+++ crawled_data/user_info/copy_script.py
@@ -1,23 +1,29 @@
 import os
 import shutil
 
+for typ in ['btc', 'blockchain', 'Bitcoin']:
 # folder = ['2', 'bd', 'crawled_data', 'user_info', 'blockchain']
-folder = ['vatly2020', 'bd', 'crawled_data', 'user_info', 'Bitcoin']
-folder_src = os.path.join(*folder)
-lf_src = os.listdir(folder_src)
+    folder = ['huynv2002', 'bd', 'crawled_data', 'user_info', typ]
+    folder_src = os.path.join(*folder)
+    lf_src = os.listdir(folder_src)
 
-folder_dst = folder[-1]
-print(f"Num file in source folder = {len(lf_src)}")
-print(f"Num file in destination folder = {len(os.listdir(folder_dst))}")
+    folder_dst = folder[-1]
 
-count = 0
-for name in lf_src:
-    src_path = os.path.join(folder_src, name)
-    dst_path = os.path.join(folder_dst, name)
-    # if not os.path.exists(dst_path) and os.stat(src_path).st_size > 1024 * 1.5: #file lớn hơn 1.5kB
-    if not os.path.exists(dst_path):
-        count += 1
-        shutil.copyfile(src_path, dst_path)
+    old_num_dst = len(os.listdir(folder_dst))
+    count = 0
+    for name in lf_src:
+        src_path = os.path.join(folder_src, name)
+        dst_path = os.path.join(folder_dst, name)
+        # if not os.path.exists(dst_path) and os.stat(src_path).st_size > 1024 * 1.5: #file lớn hơn 1.5kB
+        if not os.path.exists(dst_path):
+            count += 1
+            shutil.copyfile(src_path, dst_path)
 
-print(f"Số file đã copy = {count}")
-print(f"Num files in destination folder = {len(os.listdir(folder_dst))}")
+    if count != 0:
+        print(typ)
+        print(f"Num file in source folder = {len(lf_src)}")
+        print(f"Num file in destination folder = {old_num_dst}")
+        print(f"Số file đã copy = {count}")
+        print(f"Num files in destination folder = {len(os.listdir(folder_dst))}")
+
+print("DONE")
