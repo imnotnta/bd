f626067a79342b168776588ed5f5916032544c0e
huydsai02 <huydsai02@gmail.com>
2023-11-11 17:52:35 +0700

    update git

--- crawled_data/user_info/update_dict.py
+++ crawled_data/user_info/update_dict.py
@@ -2,7 +2,7 @@ import pickle, os
 
 ######################## Đổi thông tin ở đây ####################
 dict_path = 'info.pkl'
-folders = ['huydsai02', 'vatly2020', 'nvhuy126']
+folders = ['aehuyhoang1', 'choiiiiii307', 'huymovie2002', 'huynv2002', 'nvhuy127', 'nvhuy128']
 #################################################################
 
 def save_dict(dct, path):
