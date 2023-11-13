4ddabd10a0c7dc6d260147c77c039f89ee6053d3
huydsai02 <huydsai02@gmail.com>
2023-11-13 21:56:16 +0700

    update

--- crawled_data/user_info/update_dict.py
+++ crawled_data/user_info/update_dict.py
@@ -3,6 +3,7 @@ import pickle, os
 ######################## Đổi thông tin ở đây ####################
 dict_path = 'info.pkl'
 folders = ['huydsai02', 'huymovie2002', 'nvhuy126', 'vatly2020']
+# folders = ['aehuyhoang1', 'huynv2002', 'nvhuy127', 'nvhuy128']
 #################################################################
 
 def save_dict(dct, path):
