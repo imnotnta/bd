6e22fe57e73f29e20bb7bf8041ff8a95dbf4dd1b
huydsai02 <huydsai02@gmail.com>
2023-11-16 00:21:38 +0700

    update crypto

--- crawled_data/user_info/copy_script.py
+++ crawled_data/user_info/copy_script.py
@@ -1,31 +1,34 @@
 import os
 import shutil
 
-for typ in ['btc', 'blockchain', 'Bitcoin']:
-# folder = ['2', 'bd', 'crawled_data', 'user_info', 'blockchain']
-    folder = ['nvhuy128', typ]
-    folder_src = os.path.join(*folder)
-    if not os.path.exists(folder_src):
-        continue
-    lf_src = os.listdir(folder_src)
+accounts = ['aehuyhoang1', 'huynv2002', 'nvhuy127', 'nvhuy128']
 
-    folder_dst = folder[-1]
+for account in accounts:
+    for typ in ['btc', 'blockchain', 'Bitcoin', 'crypto']:
+    # folder = ['2', 'bd', 'crawled_data', 'user_info', 'blockchain']
+        folder = [account, typ]
+        folder_src = os.path.join(*folder)
+        if not os.path.exists(folder_src):
+            continue
+        lf_src = os.listdir(folder_src)
 
-    old_num_dst = len(os.listdir(folder_dst))
-    count = 0
-    for name in lf_src:
-        src_path = os.path.join(folder_src, name)
-        dst_path = os.path.join(folder_dst, name)
-        # if not os.path.exists(dst_path) and os.stat(src_path).st_size > 1024 * 1.5: #file lớn hơn 1.5kB
-        if not os.path.exists(dst_path):
-            count += 1
-            shutil.copyfile(src_path, dst_path)
+        folder_dst = folder[-1]
 
-    if count != 0:
-        print(typ)
-        print(f"Num file in source folder = {len(lf_src)}")
-        print(f"Num file in destination folder = {old_num_dst}")
-        print(f"Số file đã copy = {count}")
-        print(f"Num files in destination folder = {len(os.listdir(folder_dst))}")
+        old_num_dst = len(os.listdir(folder_dst))
+        count = 0
+        for name in lf_src:
+            src_path = os.path.join(folder_src, name)
+            dst_path = os.path.join(folder_dst, name)
+            # if not os.path.exists(dst_path) and os.stat(src_path).st_size > 1024 * 1.5: #file lớn hơn 1.5kB
+            if not os.path.exists(dst_path):
+                count += 1
+                shutil.copyfile(src_path, dst_path)
+
+        if count != 0:
+            print(typ)
+            print(f"Num file in source folder = {len(lf_src)}")
+            print(f"Num file in destination folder = {old_num_dst}")
+            print(f"Số file đã copy = {count}")
+            print(f"Num files in destination folder = {len(os.listdir(folder_dst))}")
 
 print("DONE")
